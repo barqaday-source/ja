28fcdfd64122c44216292d604a107f3311d8ad50
barq <barqaday@gmail.com>
2026-09-06 02:54:46 +0300

    Delete jabilak-web-android-clean directory

--- jabilak-web-android-clean/constants/brand.ts
+++
@@ -1,5 +0,0 @@
-export const BRAND = {
-  name: "جَايَبْلَك",
-  tagline: "جَايَبْلَك، كل المتاجر في مكان واحد",
-  shortTagline: "جَايَبْلَك، كل المتاجر في مكان واحد",
-} as const;
--- jabilak-web-android-clean/lib/data/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./models";
-export * from "./mappers";
-export * from "./repositories";
-export * from "./upload-validation";
-export * from "./realtime-contracts";
-export * from "./delete-helpers";
